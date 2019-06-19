4b8212076ae577941cf83e6903b53fb4ae5ddf90
МедведевС <e.eagle@mail.ru>
2019-06-19 21:09:38 +0600

    add background

--- app/src/main/java/com/example/lesson2/View/MainActivity.java
+++ app/src/main/java/com/example/lesson2/View/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lesson2;
+package com.example.lesson2.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.lesson2.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
---
+++ app/src/main/java/com/example/lesson2/View/MyActivity2.java
@@ -0,0 +1,16 @@
+package com.example.lesson2.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lesson2.R;
+
+public class MyActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my2);
+    }
+}
